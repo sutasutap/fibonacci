cababae4753c3a6747e9adc244794802d2bdddf4
Mieko Kobayashi <sutasutap@gmail.com>
2020-09-08 11:10:17 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n){
-    if (n===0){
-        return 0;
-    }else if (n===1){
-        return 1;
+    //メモにデータがあればそれを使う
+    if (memo.has(n)){ 
+        return memo.get(n);
+    }else {
+    //メモにデータがないので計算する
+    let value = fib(n-1) +fib(n-2);
+    memo.set(n,value);    
+    return value;
     }
-    return fib(n-1) +fib(n-2);
 }
 for(let i = 1; i <= 40; i++){
     fib(i)
