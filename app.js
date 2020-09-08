d51d726470161d5c587d87d982fe4f5406fd3cb4
Mieko Kobayashi <sutasutap@gmail.com>
2020-09-08 10:44:45 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n){
+    if (n===0){
+        return 0;
+    }else if (n===1){
+        return 1;
+    }
+    return fib(n-1) +fib(n-2);
+}
+for(let i = 1; i <= 40; i++){
+    fib(i)
+    console.log(i + "フィボナッチ数は" + fib(i) +"です。")
+}
